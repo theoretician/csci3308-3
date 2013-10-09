dc75ae680aeefce02af3568750dae4f0d3d63dce
William Farmer <willzfarmer@gmail.com>
2013-10-09 13:27:18 -0600

    Hopefully B

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
 
+  def ascending
+      @movies = Movie.order("title ASC")
+      render "index"
+  end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,8 +12,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all(:order => @sorted)
-    @highlight = @sorted_field
+    @movies = Movie.all
   end
 
   def new
